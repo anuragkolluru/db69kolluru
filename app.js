55395873418adae87abac566ca4fbf5b767413f0
anuragkolluru <anuragcool.kolluru87@gmail.com>
2021-11-19 02:19:01 -0600

    new changes

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-const connectionString =  
-process.env.MONGO_CON 
-mongoose = require('mongoose'); 
-mongoose.connect(connectionString,  {useNewUrlParser: true, useUnifiedTopology: true}); 
+const connectionString =
+  process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,35 +20,67 @@ var animal = require("./models/animal");
 var resourceRouter = require('./routes/resource');
 var app = express();
 
+
+
+
 // We can seed the collection if needed on server start
-async function recreateDB(){
+async function recreateDB() {
   // Delete everything 
   await animal.deleteMany();
-  let instance1 = new animal({ctype:"bear", cnum:'4', location:'location'});
-  instance1.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("First object saved")
+  let instance1 = new animal({ ctype: "bear", cnum: '4', location: 'location' });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
   });
 
-  let instance2 = new animal({ctype:"chimpu", cnum:'5', location:'location'});
-  instance2.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("Second object saved")
+  let instance2 = new animal({ ctype: "chimpu", cnum: '5', location: 'location' });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved")
   });
-  
-  let instance3 = new animal({ctype:"panda", cnum:'3', location:'location'});
-  instance3.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("Third object saved")
+
+  let instance3 = new animal({ ctype: "panda", cnum: '3', location: 'location' });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved")
   });
-  }
-  
-  let reseed = true;
-  if (reseed) { recreateDB();}
+}
+
+let reseed = true;
+if (reseed) { recreateDB(); }
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }))
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -62,12 +96,12 @@ app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -82,9 +116,10 @@ module.exports = app;
 
 
 //Get the default connection 
-var db = mongoose.connection; 
- 
+var db = mongoose.connection;
+
 //Bind connection to error event  
-db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
-db.once("open", function(){ 
- console.log("Connection to DB succeeded")}); 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
